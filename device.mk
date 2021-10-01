a1db5578c73c45c47cfb792635ffabb510e9da3c
CaptainThrowback <captainthrowback@hotmail.com>
2021-10-01 23:39:02 +0800

    cas: Build fastboot & apex

--- device.mk
+++ device.mk
@@ -8,3 +8,13 @@ LOCAL_PATH := device/xiaomi/cas
 
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# Apex libraries
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
