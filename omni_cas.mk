99eefe33f6ff403919763907fc021610c9f067fc
Xayah <zds1249475336@gmail.com>
2021-11-02 13:36:22 +0800

    cas: Uprev to A11

--- omni_cas.mk
+++ omni_cas.mk
@@ -13,8 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cas
