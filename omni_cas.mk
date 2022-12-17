75028910117ac0ebc32986d79f1100051b7e9006
Sergeant Kakashi <111608769+sergeantkakashi@users.noreply.github.com>
2022-12-18 00:23:11 +0100

    Update omni_cas.mk

--- omni_cas.mk
+++ omni_cas.mk
@@ -9,16 +9,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from cas device
-$(call inherit-product, device/xiaomi/cas/device.mk)
+# Inherit from NX659J device
+$(call inherit-product, device/nubia/NX659J/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := cas
-PRODUCT_NAME := omni_cas
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := M2007J1SC
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_DEVICE := NX659J
+PRODUCT_NAME := omni_NX659J
+PRODUCT_BRAND := nubia
+PRODUCT_MODEL := NX659J
+PRODUCT_MANUFACTURER := nubia
